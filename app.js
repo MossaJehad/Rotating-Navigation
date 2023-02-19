0312f0fe798021af6745d93ab363020685fd59c2
MossaJehad <Moosa.Jehad65@gmail.com>
2023-02-19 20:43:18 +0300

    fix buttons & transitions

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener('click', () => {
+  container.classList.add('show-nav')
+  close.classList.remove('displaynone')
+  open.classList.add('displaynone')
+})
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener('click', () => {
+  container.classList.remove('show-nav')
+  open.classList.remove('displaynone')
+  close.classList.add('displaynone')
+})
